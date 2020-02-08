a741a60fedd0cdedf7bab8429350699f12e3c3db
David Zhuang <dvdzhuang@gmail.com>
2020-02-08 04:24:38 -0500

    scripts

---
+++ Assets/Scripts/BeatmapReader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class BeatmapReader
+{
+    public void read(string path)
+    {
+
+    }
+
+    public List<Click> getClicks()
+    {
+
+    }
+
+    public List<Slider> getSliders()
+    {
+
+    }
+
+    public float getRadius()
+    {
+
+    }
+
+    public float getApproach()
+    {
+
+    }
+
+    public float getDrain()
+    {
+
+    }
+}
