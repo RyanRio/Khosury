7889678430c07b2fd2f76ee521d646f7f7fd1d05
David Zhuang <dvdzhuang@gmail.com>
2020-02-08 05:12:58 -0500

    map shit

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,20 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Map : MonoBehaviour
 {
     List<Click> clicks;
     List<Slider> sliders;
     public string beatmapPath;
-    BeatmapReader reader;
+    public BeatmapReader reader;
     float radius;
     float approach;
     float drain;
     float tolerance;
 
-    float SCREEN_HEIGHT = Camera.main.orthographicSize * 2;
-    float SCREEN_WIDTH = screenHeight * Screen.width / Screen.height;
+    static float SCREEN_HEIGHT = Camera.main.orthographicSize * 2;
+    static float SCREEN_WIDTH = SCREEN_HEIGHT * Screen.width / Screen.height;
 
 
     // Start is called before the first frame update
@@ -32,26 +33,74 @@ public class Map : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        clicks = clicks.Where(c => !isTooOld(c)).ToList();
+        sliders = sliders.Where(s => !isTooOld(s)).ToList();
     }
 
     void handleClick(Vector2 pos)
     {
+        // todo sliders
+        foreach (Click c in clicks)
+        {
+            float dsq = Vector2.SqrMagnitude(new Vector2(c.x, c.y) - pos);
+            if (dsq <= radius * radius)
+            {
+                if (Mathf.Abs(Time.time / 1000f - c.time) <= 2 * tolerance)
+                {
+                    // todo notify success of c
+                    break;
+                }
+            } 
+            else
+            {
+                // todo notify fail of c
+                break;
+            }
+        }
+    }
 
+    bool isVisible(Click c)
+    {
+        return Time.time / 1000f >= c.time - approach && Time.time / 1000f <= c.time + tolerance;
+    }
+
+    bool isVisible(Slider s)
+    {
+        return Time.time / 1000f >= s.startTime - approach && Time.time / 1000f <= s.endTime + tolerance;
+    }
+
+    bool isTooOld(Click c)
+    {
+        return Time.time / 1000f > c.time + tolerance;
+    }
+
+    bool isTooOld(Slider s)
+    {
+        return Time.time / 1000f > s.endTime + tolerance;
+    }
+
+    List<Click> visibleClicks()
+    {
+        return clicks.Where(c => isVisible(c)).ToList();
+    }
+
+    List<Slider> visibleSliders()
+    {
+        return sliders.Where(s => isVisible(s)).ToList();
     }
 
     Vector2 toBeatmapCoords(Vector2 pos)
     {
         
-        float x = pos.x * 16 / SCREEN_WIDTH + 8;
-        float y = pos.y * 9 / SCREEN_HEIGHT + 4.5;
+        float x = pos.x * 16f / SCREEN_WIDTH + 8f;
+        float y = pos.y * 9f / SCREEN_HEIGHT + 4.5f;
         return new Vector2(x, y);
     }
 
     Vector2 toUnityCoords(Vector2 pos)
     {
-        float x = (pos.x - 8) * SCREEN_WIDTH / 16;
-        float y = (pox.y - 4.5) * SCREEN_HEIGHT / 9;
+        float x = (pos.x - 8f) * SCREEN_WIDTH / 16f;
+        float y = (pos.y - 4.5f) * SCREEN_HEIGHT / 9f;
         return new Vector2(x, y);
     }
 }
