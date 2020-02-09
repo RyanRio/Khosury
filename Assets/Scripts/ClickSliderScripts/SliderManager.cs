48aeec48dfe41e85a42352ba06c780edebba7caf
RyanRio <thermitchellman@gmail.com>
2020-02-09 09:54:46 -0500

    json update

--- Assets/Scripts/ClickSliderScripts/SliderManager.cs
+++ Assets/Scripts/ClickSliderScripts/SliderManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// manages sliders and their movement
 public class SliderManager : MonoBehaviour
 {
     Dictionary<int, SliderBoth> sliders;
